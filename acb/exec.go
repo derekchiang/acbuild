b3182bed13021fcc3a634293c4ed7790c49790fe
Derek Chiang <derekchiang93@gmail.com>
2015-06-21 07:44:53 +0000

    Make use of systemd-nspawn

--- acb/exec.go
+++ acb/exec.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/coreos/rkt/store"
 	"github.com/spf13/cobra"
@@ -22,16 +23,18 @@ var (
 )
 
 func runExec(cmd *cobra.Command, args []string) {
-	if flagIn == "" || flagOut == "" {
-		stderr("--in and --out both need to be set")
+	if flagIn == "" || flagCmd == "" || flagOut == "" {
+		stderr("--in, --cmd, and --out need to be set")
 		return
 	}
 
+	// Open the ACI store
 	s, err := store.NewStore(storeDir)
 	if err != nil {
 		stderr("Could not open a new ACI store: %s", err)
 	}
 
+	// Put the ACI into the store
 	f, err := os.Open(flagIn)
 	if err != nil {
 		stderr("Could not open ACI image: %s", err)
@@ -42,21 +45,31 @@ func runExec(cmd *cobra.Command, args []string) {
 		stderr("Could not open ACI: %s", key)
 	}
 
+	// Render the ACI
 	err = s.RenderTreeStore(key, false)
 	if err != nil {
 		stderr("Could not render tree store: %s", err)
 	}
 
+	// Copy the rendered ACI into a temporary directory for manipulation
 	rootfsPath := s.GetTreeStoreRootFS(key)
-	tmpDir, err := ioutil.TempDir("", "acbuild")
+	tmpDir, err := ioutil.TempDir("", "acbuild-")
 	if err != nil {
 		stderr("Unable to create temporary directory: %s", err)
 	}
 
+	// Using cp for now... will eventually use pure Go to ensure portability
 	cpCmd := exec.Command("cp", "-rf", rootfsPath, tmpDir)
 	err = cpCmd.Run()
 	if err != nil {
 		stderr("Unable to copy ACI to a temporary directory: %s", err)
+	}
+
+	// Use systemd-nspawn to run the given command
+	nspawnCmd := exec.Command("systemd-nspawn", "-D", filepath.Join(tmpDir, "rootfs"), flagCmd)
+	err = nspawnCmd.Run()
+	if err != nil {
+		stderr("Unable to run systemd-nspawn: %s", err)
 	}
 }
 
