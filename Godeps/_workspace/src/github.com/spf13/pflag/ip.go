06098fdbe585735ed675d308d0b8a79d42678587
Derek Chiang <derekchiang93@gmail.com>
2015-07-20 05:33:32 +0000

    Update dependencies

--- Godeps/_workspace/src/github.com/spf13/pflag/ip.go
+++ Godeps/_workspace/src/github.com/spf13/pflag/ip.go
@@ -22,12 +22,26 @@ func (i *ipValue) Set(s string) error {
 	*i = ipValue(ip)
 	return nil
 }
-func (i *ipValue) Get() interface{} {
-	return net.IP(*i)
-}
 
 func (i *ipValue) Type() string {
 	return "ip"
+}
+
+func ipConv(sval string) (interface{}, error) {
+	ip := net.ParseIP(sval)
+	if ip != nil {
+		return ip, nil
+	}
+	return nil, fmt.Errorf("invalid string being converted to IP address: %s", sval)
+}
+
+// GetIP return the net.IP value of a flag with the given name
+func (f *FlagSet) GetIP(name string) (net.IP, error) {
+	val, err := f.getFlagType(name, "ip", ipConv)
+	if err != nil {
+		return nil, err
+	}
+	return val.(net.IP), nil
 }
 
 // IPVar defines an net.IP flag with specified name, default value, and usage string.
