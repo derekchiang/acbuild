26a301a381bdece7890286472b07cb3c7cc34d47
Derek Chiang <derekchiang93@gmail.com>
2015-07-27 01:41:28 +0000

    Adding test

--- acb/add_test.go
+++ acb/add_test.go
@@ -2,9 +2,13 @@ package acb
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/appc/spec/aci"
+	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/appc/spec/schema"
+	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/appc/spec/schema/types"
 	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/stretchr/testify/assert"
 
 	"github.com/appc/acbuild/common"
@@ -18,7 +22,35 @@ func TestAdd(t *testing.T) {
 	tmpDir, err := ioutil.TempDir("", "")
 	assert.NoError(t, err, "error creating tmp dir")
 
-	output := filepath.Join(tmpDir, "go-with-code.aci")
+	outputACI := filepath.Join(tmpDir, "go-with-code.aci")
 	outputImageName := "go-with-code"
-	assert.NoError(t, Add(s, []string{fixtures.CodeACI, fixtures.GoACI}, output, outputImageName), "error with Add")
+	assert.NoError(t, Add(s, []string{fixtures.CodeACI, fixtures.GoACI}, outputACI, outputImageName), "error with Add")
+
+	f, err := os.Open(outputACI)
+	assert.NoError(t, err, "error opening output ACI")
+
+	outputIm, err := aci.ManifestFromImage(f)
+	assert.NoError(t, err, "error extracting manifest from output ACI")
+	assert.Equal(t, outputIm.ACKind, schema.ImageManifestKind, "wrong AC kind")
+	assert.Equal(t, outputIm.ACVersion, schema.AppContainerVersion, "wrong AC version")
+	assert.Equal(t, outputIm.Name, types.ACIdentifier("go-with-code"))
+	f.Close()
+
+	assert.Len(t, outputIm.Dependencies, 2, "should only have two layers")
+
+	assert.Equal(t, outputIm.Dependencies[0].ImageName,
+		fixtures.CodeACIName, "names don't match")
+
+	key, err := s.ResolveKey(outputIm.Dependencies[0].ImageID.String())
+	assert.NoError(t, err, "error resolving key ", key)
+
+	assert.Equal(t, key, fixtures.CodeACIKey, "keys don't match")
+
+	assert.Equal(t, outputIm.Dependencies[1].ImageName,
+		fixtures.GoACIName, "names don't match")
+
+	key, err = s.ResolveKey(outputIm.Dependencies[1].ImageID.String())
+	assert.NoError(t, err, "error resolving key ", key)
+
+	assert.Equal(t, key, fixtures.GoACIKey, "keys don't match")
 }
--- internal/fixtures/fixtures.go
+++ internal/fixtures/fixtures.go
@@ -1,6 +1,72 @@
 package fixtures
 
-const (
-	CodeACI = "code.aci"
-	GoACI   = "go.aci"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/appc/spec/aci"
+	"github.com/appc/acbuild/Godeps/_workspace/src/github.com/appc/spec/schema/types"
+
+	"github.com/appc/acbuild/common"
 )
+
+var (
+	CodeACI     = "code.aci"
+	CodeACIKey  string
+	CodeACIName types.ACIdentifier
+
+	GoACI     = "go.aci"
+	GoACIKey  string
+	GoACIName types.ACIdentifier
+)
+
+func getKey(aci string) string {
+	s, err := common.GetTmpStore()
+	if err != nil {
+		panic("error opening temp store %v")
+	}
+
+	f, err := os.Open(aci)
+	if err != nil {
+		panic(fmt.Errorf("error opening aci %s: %v", aci, err))
+	}
+	defer f.Close()
+
+	key, err := s.WriteACI(f, false)
+	if err != nil {
+		panic(fmt.Errorf("error extracting key from aci %s: %v", aci, err))
+	}
+
+	return key
+}
+
+func getName(aciPath string) types.ACIdentifier {
+	f, err := os.Open(aciPath)
+	if err != nil {
+		panic(fmt.Errorf("error opening aci %s: %v", aciPath, err))
+	}
+	defer f.Close()
+
+	im, err := aci.ManifestFromImage(f)
+	if err != nil {
+		panic(fmt.Errorf("error extracting manifest from aci %s: %v", aciPath, err))
+	}
+
+	return im.Name
+}
+
+func init() {
+	// A hack to get the path to the "fixtures" directory
+	_, filename, _, _ := runtime.Caller(1)
+	dir := filepath.Dir(filename)
+
+	CodeACI = filepath.Join(dir, CodeACI)
+	CodeACIName = getName(CodeACI)
+	CodeACIKey = getKey(CodeACI)
+
+	GoACI = filepath.Join(dir, GoACI)
+	GoACIName = getName(GoACI)
+	GoACIKey = getKey(GoACI)
+}
